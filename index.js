ccba3ec1370c3dbc68ca136ce92d3bcf23c100c7
Caroline Chen <carolinecaroline@gmail.com>
2017-05-22 11:27:51 -0400

    seeding database

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 
-const parser  = require("body-parser");
 const hbs     = require("express-handlebars");
 const mongoose= require("./db/connection");
 
 app.set("view engine", "hbs");
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true })); 
-
+app.use("/assets", express.static("public"));
 
 app.get("/", (req, res) => {
 	res.send("Welcome to Colorswatch")
